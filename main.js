7b316b14ce39ff5276b530bbdd0ea1be90589104
Nicolás <diz.nicolasamor@gmail.com>
2017-01-27 09:09:44 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,100 @@
+/* <!--
+Motivational Quotes
+--------------------
+http://codepen.io/DizNicolasAmor/pen/OWgdBP
+  
+Author:  Diz, Nicolás Amor (https://github.com/DizNicolasAmor)
+
+This project is a challenge posed by FreeCodeCamp. 
+  
+--> */
+
+
+
+  function loadQuotes() {
+    return [{
+        "id": 1,
+        "quote": "No bird soars too high if he soars with his own wings. ",
+        "author": "William Blake",
+      }, {
+        "id": 2,
+        "quote": "In order to succeed, we must first believe that we can. ",
+        "author": "Nikos Kazantzakis",
+      }, {
+        "id": 3,
+        "quote": "Quality is not an act, it is a habit. ",
+        "author": "Aristotle",
+      }, {
+        "id": 4,
+        "quote": "It always seems impossible until it's done. ",
+        "author": "Nelson Mandela",
+      }, {
+        "id": 5,
+        "quote": "Problems are not stop signs, they are guidelines.  ",
+        "author": "Robert H. Schuller",
+      }, {
+        "id": 6,
+        "quote": "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time. ",
+        "author": "Thomas A. Edison",
+      }, {
+        "id": 7,
+        "quote": "A creative man is motivated by the desire to achieve, not by the desire to beat others. ",
+        "author": "Ayn Rand",
+      }, {
+        "id": 8,
+        "quote": "Love is the only force capable of transforming an enemy into a friend. ",
+        "author": "Martin Luther King, Jr.",
+      }, {
+        "id": 9,
+        "quote": "Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that. ",
+        "author": "Martin Luther King, Jr.",
+      }, {
+        "id": 10,
+        "quote": "The time is always right to do what is right. ",
+        "author": "Martin Luther King, Jr.",
+      }, {
+        "id": 11,
+        "quote": "Our lives begin to end the day we become silent about things that matter. ",
+        "author": "Martin Luther King, Jr.",
+      }, {
+        "id": 12,
+        "quote": "Faith is taking the first step even when you don't see the whole staircase. ",
+        "author": "Martin Luther King, Jr.",
+      }, {
+        "id": 13,
+        "quote": "If a man has not discovered something that he will die for, he isn't fit to live. ",
+        "author": "Martin Luther King, Jr.",
+      }, {
+        "id": 14,
+        "quote": "If you want to change the world, pick up your pen and write.",
+        "author": "Martin Luther",
+      }, {
+        "id": 15,
+        "quote": "Everything that is done in this world is done by hope. ",
+        "author": "Martin Luther",
+      }, {
+        "id": 16,
+        "quote": "Even if I knew that tomorrow the world would go to pieces, I would still plant my apple tree.",
+        "author": "Martin Luther",
+      }, {
+        "id": 17,
+        "quote": "The true sign of intelligence is not knowledge but imagination. ",
+        "author": "Albert Einstein",
+      }, {
+        "id": 18,
+        "quote": "When something is important enough, you do it even if the odds are not in your favor. ",
+        "author": "Elon Musk",
+      }, {
+        "id": 19,
+        "quote": "The mind is everything. What we think, we become. ",
+        "author": "Buddha",
+      }, {
+        "id": 20,
+        "quote": "Three things cannot be long hidden: the sun, the moon, and the truth. ",
+        "author": "Buddha",
+      }, {
+        
+        
+        
+      }];
+  }
