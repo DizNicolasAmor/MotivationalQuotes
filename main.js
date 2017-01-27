2d0051ccc3e066cbc5b3f52e34cd87303dda68ee
Nicolás <diz.nicolasamor@gmail.com>
2017-01-27 09:37:31 -0300

    Add quotes

--- main.js
+++ main.js
@@ -93,8 +93,113 @@ This project is a challenge posed by FreeCodeCamp.
         "quote": "Three things cannot be long hidden: the sun, the moon, and the truth. ",
         "author": "Buddha",
       }, {
-        
-        
-        
+        "id": 21,
+        "quote": "I am a slow walker, but I never walk back. ",
+        "author": "Abraham Lincoln",
+      }, {
+        "id": 22,
+        "quote": "Things may come to those who wait, but only the things left by those who hustle.",
+        "author": "Buddha",
+      }, {
+        "id": 23,
+        "quote": "You must be the change you wish to see in the world. ",
+        "author": "Mahatma Gandhi",
+      }, {
+        "id": 24,
+        "quote": "A small body of determined spirits fired by an unquenchable faith in their mission can alter the course of history. ",
+        "author": "Mahatma Gandhi",
+      }, {
+        "id": 25,
+        "quote": "A person who never made a mistake never tried anything new. ",
+        "author": "Albert Einstein",
+      }, {
+        "id": 26,
+        "quote": "Every moment and every event of every man's life on earth plants something in his soul. ",
+        "author": "Thomas Merton",
+      }, {
+        "id": 27,
+        "quote": "I believe there's an inner power that makes winners or losers. And the winners are the ones who really listen to the truth of their hearts. ",
+        "author": "Sylvester Stallone",
+      }, {
+        "id": 28,
+        "quote": "I believe every human has a finite number of heartbeats. I don't intend to waste any of mine. ",
+        "author": "Neil Armstrong",
+      }, {
+        "id": 29,
+        "quote": "Ideas shape the course of history. ",
+        "author": "John Maynard Keynes",
+      }, {
+        "id": 30,
+        "quote": "What we need is more people who specialize in the impossible. ",
+        "author": "Theodore Roethke",
+      }, {
+        "id": 31,
+        "quote": "Courage, my friends; it is not too late to build a better world. ",
+        "author": "Tommy Douglas",
+      }, {
+        "id": 32,
+        "quote": "Setting goals is the first step in turning the invisible into the visible. ",
+        "author": "Tony Robbins",
+      }, {
+        "id": 33,
+        "quote": "It is in your moments of decision that your destiny is shaped. ",
+        "author": "Tony Robbins",
+      }, {
+        "id": 34,
+        "quote": "I challenge you to make your life a masterpiece. I challenge you to join the ranks of those people who live what they teach, who walk their talk. ",
+        "author": "Tony Robbins",
+      }, {
+        "id": 35,
+        "quote": "How am I going to live today in order to create the tomorrow I'm committed to? ",
+        "author": "Tony Robbins",
+      }, {
+        "id": 36,
+        "quote": "Successful people ask better questions, and as a result, they get better answers. ",
+        "author": "Tony Robbins",
+      }, {
+        "id": 37,
+        "quote": "Passion is the genesis of genius. ",
+        "author": "Tony Robbins",
+      }, {
+        "id": 38,
+        "quote": "The secret of success is learning how to use pain and pleasure instead of having pain and pleasure use you. If you do that, you're in control of your life. If you don't, life controls you. ",
+        "author": "Tony Robbins",
+      }, {
+        "id": 39,
+        "quote": "For changes to be of any true value, they've got to be lasting and consistent. ",
+        "author": "Tony Robbins",
+      }, {
+        "id": 40,
+        "quote": "You see, in life, lots of people know what to do, but few people actually do what they know. Knowing is not enough! You must take action. ",
+        "author": "Tony Robbins",
+      }, {
+        "id": 41,
+        "quote": "You always succeed in producing a result. ",
+        "author": "Tony Robbins",
+      }, {
+        "id": 42,
+        "quote": "A real decision is measured by the fact that you've taken a new action. If there's no action, you haven't truly decided. ",
+        "author": "Tony Robbins",
+      }, {
+        "id": 43,
+        "quote": "We can change our lives. We can do, have, and be exactly what we wish. 
+",
+        "author": "Tony Robbins",
+      }, {
+        "id": 44,
+        "quote": "It is by acts and not by ideas that people live. ",
+        "author": "Anatole France",
+      }, {
+        "id": 45,
+        "quote": "Great hopes make great men. ",
+        "author": "Thomas Fuller",
+      }, {
+        "id": 46,
+        "quote": "I think it is possible for ordinary people to choose to be extraordinary. ",
+        "author": "Elon Musk",
+      }, {
+        "id": 47,
+        "quote": "The people who are crazy enough to think they can change the world, are the ones who do. ",
+        "author": "Steve Jobs",
       }];
   }
